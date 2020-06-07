bd677b5d5bcd1347e421b9a5a6644346f6986bcb
Arvydas Silanskas <nma.arvydas.silanskas@gmail.com>
2020-06-07 16:38:16 +0300

    update implementation and tests to match revised spec

--- srfi-194-impl.scm
+++ srfi-194-impl.scm
@@ -1,143 +1,123 @@
 ;;
+;; Parameters & syntax
+;;
+(define current-random-source (make-parameter default-random-source))
+
+(define-syntax with-random-source
+  (syntax-rules ()
+    ((_ random-source proc arg ...)
+     (begin
+       (unless (random-source? random-source)
+         (error "expected random source"))
+       (parameterize ((current-random-source random-source))
+                     (proc arg ...))))))
+
+;;
 ;; Primitive randoms
 ;;
 
-(define make-random-integer-generator
-  (case-lambda
-    ((low-bound up-bound)
-     (make-random-integer-generator default-random-source low-bound up-bound))
-    ((rand-src low-bound up-bound)
-     (when (not (random-source? rand-src))
-       (error "expected random-source"))
+(define (make-random-integer-generator low-bound up-bound)
      (when (not (integer? low-bound))
        (error "expected integer"))
      (when (not (integer? up-bound))
        (error "expected integer"))
-     (let ((rand-int-proc (random-source-make-integers rand-src))
+     (let ((rand-int-proc (random-source-make-integers (current-random-source)))
            (range (- up-bound low-bound)))
        (lambda ()
-         (+ low-bound (rand-int-proc range)))))))
+         (+ low-bound (rand-int-proc range)))))
 
-;private
-(define (make-int-generator-maker low-bound up-bound)
-  (case-lambda
-    (() (make-random-integer-generator low-bound up-bound))
-    ((s) (make-random-integer-generator s low-bound up-bound))))
+(define (make-random-u1-generator)
+  (make-random-integer-generator 0 2))
+(define (make-random-u8-generator) 
+  (make-random-integer-generator 0 256))
+(define (make-random-s8-generator) 
+  (make-random-integer-generator -128 128))
+(define (make-random-u16-generator) 
+  (make-random-integer-generator 0 65536))
+(define (make-random-s16-generator) 
+  (make-random-integer-generator -32768 32768))
+(define (make-random-u32-generator) 
+  (make-random-integer-generator 0 (expt 2 32)))
+(define (make-random-s32-generator) 
+  (make-random-integer-generator (- (expt 2 31)) (expt 2 31)))
+(define (make-random-u64-generator) 
+  (make-random-integer-generator 0 (expt 2 64)))
+(define (make-random-s64-generator) 
+  (make-random-integer-generator (- (expt 2 63)) (expt 2 63)))
 
-(define make-random-u8-generator (make-int-generator-maker 0 256))
-(define make-random-s8-generator (make-int-generator-maker -128 128))
-(define make-random-u16-generator (make-int-generator-maker 0 65536))
-(define make-random-s16-generator (make-int-generator-maker -32768 32768))
-(define make-random-u32-generator (make-int-generator-maker 0 (expt 2 32)))
-(define make-random-s32-generator (make-int-generator-maker (- (expt 2 31)) (expt 2 31)))
-(define make-random-u64-generator (make-int-generator-maker 0 (expt 2 64)))
-(define make-random-s64-generator (make-int-generator-maker (- (expt 2 63)) (expt 2 63)))
+(define (make-random-real-generator low-bound up-bound)
+  (when (not (number? low-bound))
+    (error "expected number"))
+  (when (not (number? up-bound))
+    (error "expected number"))
+  (let ((rand-real-proc (random-source-make-reals (current-random-source)))
+        (range (- up-bound low-bound)))
+    (lambda ()
+      (+ low-bound (* range (rand-real-proc))))))
 
-(define make-random-boolean-generator
-  (case-lambda
-    (() (make-random-boolean-generator default-random-source))
-    ((s)
-     (let ((int-gen (make-random-integer-generator s 0 2)))
-      (lambda ()
-        (zero? (int-gen)))))))
+(define (make-random-boolean-generator)
+  (define u1 (make-random-u1-generator))
+  (lambda ()
+    (zero? (u1))))
 
-(define make-random-char-generator
-  (case-lambda
-    ((str)
-     (make-random-char-generator default-random-source str))
-    ((rand-src str)
-     (when (not (random-source? rand-src))
-       (error "expected random-source"))
-     (when (not (string? str))
-       (error "expected string"))
-     (let* ((int-gen (make-random-integer-generator rand-src 0 (string-length str))))
-       (lambda ()
-         (string-ref str (int-gen)))))))
+(define (make-random-char-generator str)
+  (when (not (string? str))
+    (error "expected string"))
+  (let* ((int-gen (make-random-integer-generator 0 (string-length str))))
+   (lambda ()
+     (string-ref str (int-gen)))))
 
-(define make-random-string-generator
-  (case-lambda
-    ((k str) (make-random-string-generator default-random-source k str))
-    ((s k str)
-     (let ((char-gen (make-random-char-generator s str))
-           (int-gen (make-random-integer-generator s 0 k)))
-       (lambda ()
-         (generator->string char-gen (int-gen)))))))
-
-(define make-random-real-generator
-  (case-lambda
-    ((low-bound up-bound)
-     (make-random-real-generator default-random-source low-bound up-bound))
-    ((rand-src low-bound up-bound)
-     (let* ((rand-int-proc (random-source-make-integers rand-src))
-            (steps (expt 2 32))
-            (rand-real-proc (lambda ()
-                              (/ (inexact (rand-int-proc steps))
-                                 (- steps 1))))
-            (range (- up-bound low-bound)))
-      (lambda ()
-        (+ low-bound (* (rand-real-proc) range)))))))
+(define (make-random-string-generator k str)
+  (let ((char-gen (make-random-char-generator str))
+        (int-gen (make-random-integer-generator 0 k)))
+    (lambda ()
+      (generator->string char-gen (int-gen)))))
 
 ;;
 ;; Non-uniform distributions
 ;;
 
-;; TODO import from somewhere?
 (define PI (* 4 (atan 1.0)))
+
+(define (make-bernoulli-generator p)
+  (when (or (< p 0) (> p 1))
+    (error "expected 0 <= p <= 1"))
+  (let ((rand-real-proc (random-source-make-reals (current-random-source)))) 
+   (lambda ()
+     (if (<= (rand-real-proc) p)
+         0
+         1))))
 
 (define make-normal-generator
   (case-lambda
     (()
-     (make-normal-generator default-random-source 0.0 1.0))
-    ((arg1)
-     (cond
-       ((random-source? arg1)
-        (make-normal-generator arg1 0.0 1.0))
-       (else (make-normal-generator default-random-source arg1 1.0))))
-    ((arg1 arg2)
-     (cond
-       ((and (random-source? arg1)
-             (number? arg2))
-        (make-normal-generator arg1 arg2 1.0))
-       ((and (number? arg1)
-             (number? arg2))
-        (make-normal-generator default-random-source arg1 arg2))
-       (else (error "expected random-source and mean, or mean and standard deviation"))))
-    ((rand-src mean deviation)
-     (let ((rand-real-proc (random-source-make-reals rand-src)))
+     (make-normal-generator 0.0 1.0))
+    ((mean)
+     (make-normal-generator mean 1.0))
+    ((mean deviation)
+     (let ((rand-real-proc (random-source-make-reals (current-random-source))))
       (lambda ()
         ;;Box-Muller
         (let ((r (sqrt (* -2 (log (rand-real-proc)))))
               (theta (* 2 PI (rand-real-proc))))
           (+ mean (* deviation r (sin theta)))))))))
 
-(define make-exponential-generator
-  (case-lambda
-    ((mean)
-     (make-exponential-generator default-random-source mean))
-    ((rand-src mean)
-     (let ((rand-real-proc (random-source-make-reals rand-src)))
-      (lambda ()
-       (- (* mean (log (rand-real-proc)))))))))
+(define (make-exponential-generator mean)
+  (let ((rand-real-proc (random-source-make-reals (current-random-source))))
+   (lambda ()
+     (- (* mean (log (rand-real-proc)))))))
 
-(define make-geometric-generator
-  (case-lambda
-    ((p)
-     (make-geometric-generator default-random-source p))
-    ((rand-src p)
-     (let ((c (/ (log (- 1.0 p))))
-           (rand-real-proc (random-source-make-reals rand-src)))
-      (lambda ()
-        (ceiling (* c (log (rand-real-proc)))))))))
+(define (make-geometric-generator p)
+  (let ((c (/ (log (- 1.0 p))))
+        (rand-real-proc (random-source-make-reals (current-random-source))))
+    (lambda ()
+      (ceiling (* c (log (rand-real-proc)))))))
 
-(define make-poisson-generator
-  (case-lambda
-    ((L)
-     (make-poisson-generator default-random-source L))
-    ((rand-src L)
-     (let ((rand-real-proc (random-source-make-reals rand-src)))
-      (if (< L 36)
-          (make-poisson/small rand-real-proc L)
-          (make-poisson/large rand-real-proc L))))))
+(define (make-poisson-generator L)
+  (let ((rand-real-proc (random-source-make-reals (current-random-source))))
+   (if (< L 36)
+       (make-poisson/small rand-real-proc L)
+       (make-poisson/large rand-real-proc L))))
 
 ;private
 (define (make-poisson/small rand-real-proc L)
@@ -204,16 +184,10 @@
               (/ 1.0 (* x 12.0)))))))
 
 
-(define (gsampling . args)
-  (cond
-    ((null? args) (gsampling* default-random-source '()))
-    ((random-source? (car args)) (gsampling* (car args) (cdr args)))
-    (else (gsampling* default-random-source args))))
 
-;private
-(define (gsampling* s generators-lst)
+(define (gsampling . generators-lst)
   (let ((gen-vec (list->vector generators-lst))
-        (rand-int-proc (random-source-make-integers s)))
+        (rand-int-proc (random-source-make-integers (current-random-source))))
 
        ;remove exhausted generator at index
        (define (remove-gen index)
@@ -246,12 +220,11 @@
              (pick)))))
 
 (define (gweighted-sampling . args)
-  (cond
-    ((null? args) (gweighted-sampling* default-random-source '()))
-    ((random-source? (car args)) (gweighted-sampling* (car args) (group-weights-with-generators (cdr args))))
-    (else (gweighted-sampling* default-random-source (group-weights-with-generators args)))))
+  (gweighted-sampling* (group-weights-with-generators args)))
 
 ;private
+;converts flat list of interweaving weights and generators
+;to a list of weight+generator pairs
 (define (group-weights-with-generators objs)
   (let loop ((objs objs)
              (pairs '()))
@@ -269,9 +242,9 @@
                          pairs)))))))
 
 ;private
-(define (gweighted-sampling* s weight+generators-lst)
+(define (gweighted-sampling* weight+generators-lst)
   (let ((weight-sum (apply + (map car weight+generators-lst)))
-        (rand-real-proc (random-source-make-reals s)))
+        (rand-real-proc (random-source-make-reals (current-random-source))))
 
        ;randomly pick generator. If it's exhausted remove it, and pick again.
        ;returns value (or eof, if all generators are exhausted)

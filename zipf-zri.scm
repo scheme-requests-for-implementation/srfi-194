c7c87d54d77e2036548ae30053d9160fa5b67225
Linas Vepstas <linasvepstas@gmail.com>
2020-07-11 08:49:13 -0500

    Revised notation.
    Sticking with the broken notation is irritating. So fix it.

--- zipf-zri.scm
+++ zipf-zri.scm
@@ -6,65 +6,53 @@
 ;
 ; Not optimized for speed!
 ;
-; XXXXXXXXXXXXXX Attention!
-; Preliminary unit testing indicates that .. something is wrong.
-; The distributions that this generates are close to what they should
-; be, but not quite cloase enough. They have too-heavy a tail;
-; this is easily verified just by graphing for a variety of values.
-;
-; It is not currently clear if this is due to a coding bug below,
-; or an error in the algo presented in the original paper.
-;
 ; Implementation from ZRI algorithm presented in the paper:
 ; "Rejection-inversion to generate variates from monotone discrete
 ; distributions", Wolfgang Hörmann and Gerhard Derflinger
 ; ACM TOMACS 6.3 (1996): 169-184
 ;
-; Example usage:
-;    (define zgen (make-zipf-generator 50 1.01))
-;    (generator->list zgen 10)
-;
+; Hörmann and Derflinger use "q" everywhere, when they really mean "s".
+; Thier "q" is not the standard q-series deformation. Its just "s".
+; The notation in the code below has been changed to reflect
+; conventional usage.
 ;
 ; ------------------------------------------------------------
 
-; The public API
-; q must be greater than 1
+; The Hurwicz zeta distribution 1 / (k+q)^s) for 1 <= k <= n integer
+; The Zipf distribution is recovered by setting q=0.
+; The exponent `s` must be a real number greater than 1.
 ;
-; Hörmann and Derflinger use "q" everywhere, when they really mean "s".
-; The "q" here is not the standard q-series deformation. Its just "s".
+; Example usage:
+;    (define zgen (make-zipf-generator 50 1.01 0))
+;    (generator->list zgen 10)
 ;
-(define (make-zipf-generator/zri n q)
+(define (make-zipf-generator/zri n s q)
 
-	; The Hurwicz zeta offset, called "v" in the original paper.
-	; Can be any non-negative value. "v" = zero gives the canonical
-	; Zipf distribution.
-	(define vee 0)
-
-	; The hat function h(x) = 1 / (x+v) ^ q)
-	(define (hat x q)
-		(expt (+ x vee) (- q)))
+	; The hat function h(x) = 1 / (x+q) ^ s)
+	(define (hat x s)
+		(expt (+ x q) (- s)))
 
 	; The integral of hat(x)
-	; H(x) = (v+x)^{1-q} / (1-q)
+	; H(x) = (x+q)^{1-s} / (1-s)
 	; Note that H(x) is always negative.
-	(define (big-h x q)
-		(define 1mq (- 1 q))
-		(/ (expt (+ vee x) 1mq) 1mq))
+	(define (big-h x s)
+		(define 1ms (- 1 s))
+		(/ (expt (+ q x) 1ms) 1ms))
 
 	; The inverse function of H(x)
-	(define (big-h-inv x q)
-		(define 1mq (- 1 q))
-		(define omq (/ 1 1mq))
-		(- (expt (* x 1mq) omq) vee)
+	(define (big-h-inv x s)
+		(define 1ms (- 1 s))
+		(define oms (/ 1 1ms))
+		(- (expt (* x 1ms) oms) q)
 	)
 
 	; Lower and upper bounds for the uniform random generator.
-	; Note that both are negative for all values of q.
-	(define big-h-half (big-h 0.5 q))
-	(define big-h-n (big-h (+ n 0.5) q))
+	; Note that both are negative for all values of s.
+	(define big-h-half (big-h 0.5 s))
+	(define big-h-n (big-h (+ n 0.5) s))
 
 	; Rejection cut
-	(define cut (- 1 (big-h-inv (- (big-h 1.5 q) (expt (+ 1 vee) (- q))) q)))
+	(define cut (- 1 (big-h-inv (- (big-h 1.5 s) (expt (+ 1 q) (- s))) s)))
 
 	; Uniform distribution
 	(define dist (make-random-real-generator big-h-half big-h-n))
@@ -73,12 +61,12 @@
 	; otherwise return an integer between 1 and n.
 	(define (try)
 		(define u (dist))
-		(define x (big-h-inv u q))
+		(define x (big-h-inv u s))
 		(define kflt (floor (+ x 0.5)))
 		(define k (inexact->exact kflt))
 		(if (or
 			(<= (- k x) cut)
-			(>= u (- (big-h (+ k 0.5) q) (hat k q)))) k #f))
+			(>= u (- (big-h (+ k 0.5) s) (hat k s)))) k #f))
 
 	; Did we hit the dartboard? If not, try again.
 	(define (loop-until)

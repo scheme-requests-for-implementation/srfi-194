cc7bb9346d5c2f34aad359c3b9ba28e9cf2a291c
Linas Vepstas <linasvepstas@gmail.com>
2020-08-09 22:43:18 -0500

    Fix bad indentation

--- ellipsoid-test.scm
+++ ellipsoid-test.scm
@@ -33,14 +33,14 @@
 ; Vector subtraction
 ; Example usage: (vector-diff '#( 2 3) '#(0.5 0.7))
 (define (vector-sub a b)
-   (vector-map (lambda (idx ea eb) (- ea eb)) a b))
+	(vector-map (lambda (idx ea eb) (- ea eb)) a b))
 
 ; Newton differences - compute the difference between neighboring
 ; points. Assumes `pts` is a list of vectors.  Should be called with
 ; `rv` set to the null list. (tail-recursive helper)
 (define (delta pts rv)
-   (if (null? (cdr pts)) (reverse! rv)
-      (delta (cdr pts) (cons (vector-diff (car pts) (cadr pts)) rv))))
+	(if (null? (cdr pts)) (reverse! rv)
+		(delta (cdr pts) (cons (vector-diff (car pts) (cadr pts)) rv))))
 
 ; Compute sum of a list of numbers
 (define (sum lst) (fold (lambda (x sum) (+ sum x)) 0 lst))
@@ -59,37 +59,37 @@
 
 ; factorial
 (define (fact n rv)
-   (if (zero? n) rv (fact (- n 1) (* n rv))))
+	(if (zero? n) rv (fact (- n 1) (* n rv))))
 
 ; Double factorial
 ; https://en.wikipedia.org/wiki/Double_factorial
 (define (double-fact n rv)
-   (if (<= n 0) rv (double-fact (- n 2) (* n rv))))
+	(if (<= n 0) rv (double-fact (- n 2) (* n rv))))
 
 ; Complete elliptic integral per wikipedia, see the Ivorty& Bessel
 ; expansion. Here `a` and `b` are the axes.
 ; https://en.wikipedia.org/wiki/Ellipse
 (define (complete-elliptic a b)
-   (define rh (/ (- a b) (+ a b)))
-   (define h (* rh rh))
+	(define rh (/ (- a b) (+ a b)))
+	(define h (* rh rh))
 
 	(define precision 1e-10)
 
-   (define (ivory term n twon hn fact-n dfact-n sum)
-      (if (< term precision) (+ sum term)
-         ; (format #t "yo n= ~A term=~A 2^n=~A h^n=~A n!=~A n!!=~A sum=~A\n"
-         ; n term twon hn fact-n dfact-n sum)
-         (ivory
-            (/ (* dfact-n dfact-n hn) (* twon twon fact-n fact-n))
-            (+ n 1)
-            (* 2 twon)
-            (* h hn)
-            (* (+ n 1) fact-n)
-            (* (- (* 2 n) 1) dfact-n)
-            (+ term sum))))
+	(define (ivory term n twon hn fact-n dfact-n sum)
+		(if (< term precision) (+ sum term)
+			; (format #t "yo n= ~A term=~A 2^n=~A h^n=~A n!=~A n!!=~A sum=~A\n"
+			; n term twon hn fact-n dfact-n sum)
+			(ivory
+				(/ (* dfact-n dfact-n hn) (* twon twon fact-n fact-n))
+				(+ n 1)
+				(* 2 twon)
+				(* h hn)
+				(* (+ n 1) fact-n)
+				(* (- (* 2 n) 1) dfact-n)
+				(+ term sum))))
 
-   (* pi (+ a b) (+ 1 (/ h 4)
-      (ivory (/ (* h h) 64) 3 8 (* h h h) 6 3 0.0))))
+	(* pi (+ a b) (+ 1 (/ h 4)
+		(ivory (/ (* h h) 64) 3 8 (* h h h) 6 3 0.0))))
 
 ; -----------------------------------------------------------
 
